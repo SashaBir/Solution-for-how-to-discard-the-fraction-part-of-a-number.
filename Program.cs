974a68db6c8b96114c69320caad383f077362002
Sasha <64170613+SashaBir@users.noreply.github.com>
2020-12-25 16:18:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using  System;
+using System.Linq;
+
+namespace ForGitHub
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            float a = 5.454f;
+            Console.WriteLine(a);
+            a = GetShortNumber<float>(a, 2);
+            Console.WriteLine(a);
+        }
+        
+        private static T GetShortNumber<T>(dynamic anyNum, byte n = 0)
+        {
+            if (!(anyNum is float | anyNum is decimal | anyNum is double))
+                throw new Exception("This is not the right type!");
+            
+            if (n == 0)
+                return Convert.ToInt32(anyNum);
+
+            long countZero = long.Parse(1.ToString().PadRight(n + 1, '0'));;
+            
+            anyNum = (long)(anyNum * countZero);
+            anyNum = Convert.ChangeType(anyNum, typeof(T)) / countZero;
+
+            return anyNum;
+        }
+    }
+}
